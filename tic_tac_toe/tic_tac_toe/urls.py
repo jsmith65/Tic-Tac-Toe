2ed11772f8b1550e8e6789e5106f11ebfd990ff8
Jon Scott Smith <jsmith6503@gmail.com>
2013-12-18 17:07:48 -0600

    Installed the XandO main game app and hooked it up to the project.

--- tic_tac_toe/tic_tac_toe/urls.py
+++ tic_tac_toe/tic_tac_toe/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'tic_tac_toe.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', include('XandO.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
