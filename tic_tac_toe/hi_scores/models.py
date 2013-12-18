ad8fa971c9f4df1fb28785c7e616d62e1e979fbd
Jon Scott Smith <jsmith6503@gmail.com>
2013-12-18 17:52:29 -0600

    Created a model to store high scores.

--- tic_tac_toe/hi_scores/models.py
+++ tic_tac_toe/hi_scores/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Score(models.Model):
+	class Meta:
+		verbose_name = ('Score')
+		verbose_name_plural = ('Scores')
+
+	initials = models.CharField(max_length=3)
+	score = models.PositiveIntegerField()
+
+	def __unicode__(self):
+		return "%s %s" % (self.initials, self.score)
+	
